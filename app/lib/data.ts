6d4937e4f759850871dcef6e8df54c35f78b02c9
stardvst <lilia.manukyan@hotmail.com>
2024-05-30 12:05:52 +0400

    moved data fetching to components and added suspense fallbacks

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -38,12 +38,17 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    console.log('Fetching invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log('Data fetch completed after 1.5 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
